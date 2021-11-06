521fbe741992fc2293ab44dd843f1dc30b444a99
Nikolay Krashnikov <1090219@mail.ru>
2021-11-06 14:10:30 +0300

    add readme, add manifest, change cache strategy to 'network first'

---
+++ README.md
@@ -0,0 +1,15 @@
+# Offline PWA full cache (network first strategy) example
+
+## Install
+
+```sh
+npm i serve -g
+```
+
+## Run
+
+In a project directory
+
+```sh
+serve .
+```
--- sw.js
+++ sw.js
@@ -4,12 +4,12 @@ importScripts(
 
 workbox.routing.registerRoute(
   new RegExp("/.*"),
-  workbox.strategies.cacheFirst({
-    cacheName: "all-cache",
+  workbox.strategies.networkFirst({
+    cacheName: "awesome-cache",
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 100,
-        maxAgeSeconds: 365 * 24 * 60 * 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
       new workbox.cacheableResponse.Plugin({
         statuses: [0, 200],
